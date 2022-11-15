1b6289b6e3eef1cda355ea2c0303e61b549b4789
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2022-11-15 15:47:33 +0000

    criando template inicial da classe main

---
+++ src/Aplicacao.java
@@ -0,0 +1,13 @@
+/*
+ * title: Aplicacao.java
+ * subtitle: Classe main da aplicao - Avaliacao de aprendizes
+ * authors: Guilherme Monteiro de Oliveira, Gustavo Scaglione Vilmar, Pietro Tadeu Vieira Giorgi
+ * date: Tue Nov 15 15:27:52 2022
+ */
+
+class Aplicacao {
+    // Metodo main da aplicacao
+    public static void main (String[] args){
+
+    }
+}
