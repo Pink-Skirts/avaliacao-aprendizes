9b14928184d852ca13c376a4148c51051d57ce41
Gustavo Scaglione <82459122+scagl1@users.noreply.github.com>
2022-11-20 13:42:04 -0300

    modifica classe Parada, trazendo modificações que não estavam versionadas

---
+++ src/Parada.java
@@ -0,0 +1,12 @@
+/*  title:
+ *  subtitle: Laboratorio de Modelagem (LMA)
+ *  author: GM de Oliveira, GS Vilmar, PTV Giorgi
+ *  date: 18/10/2022
+ */
+
+public class Parada
+{
+    public Parada(){
+
+    }
+}
