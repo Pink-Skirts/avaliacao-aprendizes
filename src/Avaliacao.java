6495477425edb3a03a1b24b9c116277ff0397310
Gustavo Scaglione <82459122+scagl1@users.noreply.github.com>
2022-11-20 13:41:54 -0300

    modifica classe Avaliacao, trazendo modificações que não estavam versionadas

---
+++ src/Avaliacao.java
@@ -0,0 +1,16 @@
+/*  title:
+ *  subtitle: Laboratorio de Modelagem (LMA)
+ *  author: GM de Oliveira, GS Vilmar, PTV Giorgi
+ *  date: 18/10/2022
+ */
+
+public class Avaliacao
+{
+    public Avaliacao(){
+
+    }
+
+    public void avaliar(){
+
+    }
+}
